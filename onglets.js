ff7648de90dc6be152ad0040ce5c00e784fa545c
Thomas Dargon <ThomasDargon13@gmail.com>
2020-09-24 15:23:46 +0100

    Changes to JS: Restructuring the two download functions - removing unecessary code and adding clean comments.
    Changes to HTML:
    Cleaning up class names
    Changing the default svg that the pages loads with
    
    Additions:
    Adding a reset button that resets the svg and inputfields to their original values without reloading the page.

--- onglets.js
+++ onglets.js
@@ -1,25 +1,47 @@
-// ---------- Referencing the HTML elements: ----------
+/*
+ * DOM ELEMENT REFERENCES 
+ */
+
+//  input fields:
+const form = document.querySelector('form');
 const borderThicknessInput = document.querySelector('#slider');
 const borderColorInput = document.querySelector('#stroke');
 const textInput = document.querySelector('#texte');
 const textColorInput = document.querySelector('#couleur');
 const textPosition = document.querySelector('#translate');
-
-// ---------- Referencing SVG elements: ----------
+// inline svg
+const inlineSVG = document.querySelector('svg');
 const border = document.querySelector('.cls-1');
 const textDisplayed = document.querySelector('.cls-2');
+// buttons
+const dlSvgButton = document.querySelector('.downloadSVG-button');
+const dlPngButton = document.querySelector('.downloadPNG-button');
+const resetButton = document.querySelector('.reset-button');
 
-//Events listener for user inputs
-// border:
+
+/*
+ * INPUT EVENT LISTENERS
+ */
 borderColorInput.addEventListener('input', applyBorderColor);
 borderThicknessInput.addEventListener('input', applyBorderThickness);
-//text:
 textInput.addEventListener('input', changeText);
 textColorInput.addEventListener('input', applyTextColor);
 textPosition.addEventListener('input', applyTextPosition);
 
-// ---------- Functions that modify the style of the Tab ----------
-// border
+/*
+ * CLICK EVENT LISTENERS
+ */
+dlSvgButton.addEventListener('click', downloadSVG);
+dlPngButton.addEventListener('click', downloadPNG);
+resetButton.addEventListener('click', () => {
+    form.reset();
+    
+})
+
+
+/*
+ * FUNCTIONS FOR STYLING THE INLINE SVG
+ */
 function applyBorderThickness(){
     let num = Number(borderThicknessInput.value);
     border.setAttribute('stroke-width', num);
@@ -27,7 +49,6 @@ function applyBorderThickness(){
 function applyBorderColor(){
     border.setAttribute('stroke' , borderColorInput.value);
 };
-// text
 function changeText(){
     textDisplayed.innerHTML = textInput.value;
 }
@@ -36,52 +57,32 @@ function applyTextColor(){
 }
 function applyTextPosition(){
     textDisplayed.setAttribute('transform', `translate( ${textPosition.value} 28.65)`);
-
 }
 
 
-// NOW WORK on a function to export the svg and then convert it into png ? 
-// Two download buttons, one png, one svg ? -> Transparent background. 
-let inlineSVG = document.querySelector('svg');
-
-
-let dlButtonSVG = document.querySelector('.downloadSVG');
-let dlButtonPNG = document.querySelector('.downloadPNG');
-
-dlButtonSVG.addEventListener('click', function(){
-    downloadSVG('onglet.svg', inlineSVG.outerHTML)
-});
-
-function downloadSVG(filename, data){
+/*
+ * DOWNLOAD FUNCTIONS
+ */
+function downloadSVG(){
     let link = document.createElement('a');
-    let strDataURI ='data:image/svg+xml;charset=utf-8,'+ encodeURIComponent(data); // Converts svg source to URI data scheme
+    let strDataURI ='data:image/svg+xml;charset=utf-8,'+ encodeURIComponent(inlineSVG.outerHTML); // Converts svg source to URI data scheme
     link.setAttribute('href', strDataURI); 
-    link.setAttribute('download', filename);
+    link.setAttribute('download', 'onglet_' + textInput.value + '.svg');
     link.style.display = 'none';
     link.click()
 }
 
-
-function drawInlineSVG(svgElement, ctx, callback) {
-    var svgURL = new XMLSerializer().serializeToString(svgElement); // Equivalent to ligne 58 (strDataURI with encodeURIComponent(data)  )
-    var img = new Image();
-    img.onload = function() {
-      ctx.drawImage(this, 0, 0);
-      callback();
-    }
-    img.src = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(svgURL);
-  }
-
-
-dlButtonPNG.addEventListener('click', function(){
-    let canvas = document.querySelector('canvas');
-    let ctxt = canvas.getContext('2d');
-    canvas.style.display = 'none';
-    drawInlineSVG(inlineSVG, ctxt, function() {
+function downloadPNG() {
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    let img = document.createElement('img');
+    img.setAttribute('src', 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(inlineSVG.outerHTML));
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0);
         let link = document.createElement('a');
         link.setAttribute('href', canvas.toDataURL()); 
-        link.setAttribute('download', 'onglet.png');
+        link.setAttribute('download', 'onglet_' + textInput.value + '.png');
         link.style.display = 'none';
         link.click()
-      });
-});
+    };
+  }
